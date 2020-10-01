c5e30f685bb4aa5aae7eca2d9f0c4c08edd31b1a
Roshni Shetty <51188501+Roshni1999@users.noreply.github.com>
2020-10-01 16:55:02 +0530

    Update createMinModel.m

--- createMinModel.m
+++ createMinModel.m
@@ -7,15 +7,16 @@ function minModel= createMinModel(model)
 %    minModel: As computed by minReact
 
 % Computed minModel for GrowthRateCutoff = 0.05 and tol = 0
-%Jmin==1:Present in minimal model  Jmin==0: Deleted reactions
+%Jmin==1: Reactions Present in minimal model  
+%Jmin==0: Deleted reactions from model
 
 [Jmin, minReactNo] = minReact(model, 0.05, 0);
 delRxns = model.rxns(Jmin==0);
 minModel = removeRxns(model, delRxns);
 
-
+%output xml file
 xmlfile = sprintf('Min-%s',minModel.description );
-minModel = writeCbModel(minModel, 'format','sbml', 'fileName', xmlfile); %output xml file
+minModel = writeCbModel(minModel, 'format','sbml', 'fileName', xmlfile); 
 
 %output .mat file
 %minModel = writeCbModel(minModel, 'format','mat', 'fileName','minModel.mat');
